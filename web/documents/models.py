0942f53ac28bcaa7fe394555578f3d3b3649e872
Steven Joseph <stevenjose@gmail.com>
2012-03-24 23:38:27 +1100

    added stocks to model

--- web/documents/models.py
+++ web/documents/models.py
@@ -7,3 +7,13 @@ class Document(models.Model):
     symbol = models.CharField(max_length=10)
     date = models.DateField()
 
+class Stock(models.Model):
+    symbol = models.CharField(max_length=10, unique_for_date='date')
+    date = models.DateField()
+    name = models.CharField(max_length=200)
+    open = models.FloatField()
+    close = models.FloatField()
+    high  = models.FloatField()
+    low = models.FloatField()
+    volume = models.FloatField()
+
--- web/documents/views.py
+++ web/documents/views.py
@@ -1,1 +1,8 @@
 # Create your views here.
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from datetime import datetime
+from documents.models import Document
+
+def week52loosers(request):
+    
